bdd65250039f40353ab033abee682a6ebd23b1a2
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2022-12-09 19:49:43 -0300

    first commit

---
+++ ClinicaEstetica/src/clients/Clients.java
@@ -0,0 +1,49 @@
+package clients;
+
+public class Clients {
+    private String id;
+    private String name;
+    private Address address;
+    private String phone;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
